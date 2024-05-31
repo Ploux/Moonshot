3893d6480465ea1a70444eccf5eb4262572063ce
Ploux <peter@humo.golf>
2024-05-31 12:00:47 -0500

    Initial commit - add assets

--- Moonshot/ContentView.swift
+++ Moonshot/ContentView.swift
@@ -8,16 +8,15 @@
 import SwiftUI
 
 struct ContentView: View {
+    let astronauts = Bundle.main.decode("astronauts.json")
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+        Text(String(astronauts.count))
     }
 }
+
+        
+        
+
 
 #Preview {
     ContentView()
